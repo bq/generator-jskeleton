4e3262427aac1792aeed342a4ef69d1a180e36d1
Álvaro Brito <alvaro.brito@bq.com>
2015-04-27 13:02:23 +0200

    Boilerplate app ready to generate
    Change-Id: I838517fd73033cfa589acba298b70d87629635fd

---
+++ README.md
@@ -0,0 +1,23 @@
+# JSkeleton generator
+
+> Yeoman generator for JSkeleton - lets you quickly set up a project with sensible defaults and best practices.
+
+
+## Usage
+
+For step-by-step instructions on using Yeoman and this generator to build JSkeleton application from scratch see [this tutorial.](http://yeoman.io/codelab/)
+
+Install `yo`, `grunt-cli`, `bower`, `generator-jskeleton`:
+```
+npm install -g grunt-cli bower yo generator-jskeleton
+```
+
+Make a new directory, and `cd` into it:
+```
+mkdir my-new-project && cd $_
+```
+
+Run `yo jskeleton`, optionally passing an app name:
+```
+yo jskeleton [app-name]
+```
---
+++ app/index.js
@@ -0,0 +1,79 @@
+/* global require, module */
+'use strict';
+
+var generators = require('yeoman-generator'),
+    _ = require('underscore.string'),
+    path = require('path'),
+    chalk = require('chalk'),
+    yosay = require('yosay');
+
+// This mean you'll be able to use external generators inside your own, and allow user to build their app using multiple generators.
+// http://yeoman.io/blog/cleanup.html
+//
+var JSkeletonGenerator = generators.Base.extend({
+
+    init: function() {
+        // this.argument('appname', {
+        //     type: String,
+        //     required: false
+        // });
+
+        this.appName = this.appname || path.basename(process.cwd());
+        this.appName = 'jskeleton-' + _.slugify(this.appName);
+
+        this.settings = {
+            appName: this.appName
+        };
+
+        // Sets the source root in common
+        // this.sourceRoot(path.join(__dirname, '../templates/common'));
+    },
+    install: function() {
+        this.installDependencies({
+            skipInstall: this.options['skip-install'],
+            bower: true
+        });
+    },
+    end: function() {
+        this.log(yosay(chalk.green(this.appName) + ' has been built! :) ' + chalk.red('Bye!')));
+    }
+
+});
+
+JSkeletonGenerator.prototype.welcome = function welcome() {
+    if (!this.options['skip-welcome-message']) {
+        this.log(yosay('Create another ' + chalk.bgBlue('JSkeleton') + ' & ' + chalk.bgYellow('Penguin <(")') + ' webapp!'));
+    }
+};
+
+JSkeletonGenerator.prototype.scaffolding = function welcome() {
+    this.directory('src');
+    this.directory('doc');
+    this.directory('test');
+};
+
+JSkeletonGenerator.prototype.setupEnv = function welcome() {
+    this.copy('.editorconfig');
+    this.copy('gitignore', '.gitignore');
+};
+
+JSkeletonGenerator.prototype.packageFiles = function packageFiles() {
+    this.copy('Gruntfile.js', 'Gruntfile.js');
+    this.template('_bower.json', 'bower.json', this.settings);
+    this.template('_package.json', 'package.json', this.settings);
+};
+
+JSkeletonGenerator.prototype._injectDependencies = function _injectDependencies() {
+    if (this.options['skip-install']) {
+        this.log(
+            'After running `npm install & bower install`, inject your front end dependencies' +
+            '\ninto your source code by running:' +
+            '\n' +
+            '\n' + chalk.yellow.bold('grunt wiredep')
+        );
+    } else {
+        this.spawnCommand('grunt', ['wiredep']);
+    }
+};
+
+module.exports = JSkeletonGenerator;
